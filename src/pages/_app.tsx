5cd93a6ca9c5814a4d0030505b3470fffee59cea
SankhajaH <shapu953@gmial.com>
2023-05-15 13:35:16 +0530

    add: seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,26 @@ import BackgroundContainer from '@/components/background-container';
 import Footer from '@/components/common/footer';
 import Navbar from '@/components/common/navbar';
 import '@/styles/globals.css';
+import SEO from '@bradgarropy/next-seo';
 import {SessionProvider} from 'next-auth/react';
 import type {AppProps} from 'next/app';
 
 export default function App({Component, pageProps: {session, ...pageProps}}: AppProps) {
     return (
-        <SessionProvider session={session}>
-            <BackgroundContainer>
-                <Navbar />
-                <Component {...pageProps} />
-                <Footer />
-            </BackgroundContainer>
-        </SessionProvider>
+        <>
+            <SEO
+                title='Inkdrop'
+                description='Inkdrop is a elegant blogging platform built with love❤️. It allows bloggers to create, edit, and publish blog posts, as well as browse and search for posts created by other bloggers.'
+                keywords={['blog', 'website', 'inkdrop', 'micro-blogging', '#blogging']}
+                icon='/favicon.ico'
+            />
+            <SessionProvider session={session}>
+                <BackgroundContainer>
+                    <Navbar />
+                    <Component {...pageProps} />
+                    <Footer />
+                </BackgroundContainer>
+            </SessionProvider>
+        </>
     );
 }
